a6cddbf69ed89bf76329d1d8c23a88d7dfd74ef1
Marc Tismonar <tismonarmarc@gmail.com>
2024-04-30 08:42:48 +0200

    Removed the Test Files

--- ExMan/ExMan/bin/Debug/net7.0/Exercise20/CleanSolutionDir.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path $PSScriptroot -Recurse -Force  -Include '.vs', '.git', 'bin', 'obj', '.idea', '*.Dotsettings', '*.Dotsettings.user' | ForEach-Object {
-    Remove-Item -Recurse -Force -LiteralPath $_.FullName >$null
-}
